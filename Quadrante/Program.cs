062b84bc172cd03be53a27b8ccb8152bee1a344b
freitasJV <jose.freitas16@fatec.sp.gov.br>
2020-04-19 15:43:07 -0300

    Add project files.

---
+++ Quadrante/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+
+namespace Quadrante
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x, y;
+            do
+            {
+                Console.Write("Digite os valores para os eixos X e Y separados por espaço: ");
+                string[] entrada = Console.ReadLine().Split(' ');
+
+                x = double.Parse(entrada[0], CultureInfo.InvariantCulture);
+                y = double.Parse(entrada[1], CultureInfo.InvariantCulture);
+
+                if (x == 0 && y == 0)
+                    Console.WriteLine("Origem");
+                else if (x == 0)
+                    Console.WriteLine("Eixo X");
+                else if (y == 0)
+                    Console.WriteLine("Eixo Y");
+                else if (x > 0 && y > 0)
+                    Console.WriteLine("Coordenada pertencente ao primeiro quadrante");
+                else if (x < 0 && y > 0)
+                    Console.WriteLine("Coordenada pertencente ao segundo quadrante");
+                else if (x < 0 && y < 0)
+                    Console.WriteLine("Coordenada pertencente ao terceiro quadrante");
+                else if (x > 0 && y < 0)
+                    Console.WriteLine("Coordenada pertencente ao quarto quadrante");
+                else
+                    Console.WriteLine("A coordenada encontra-se em um ou nos dois eixos X e Y");
+            } while (x != 0 & y != 0);
+        }
+    }
+}
